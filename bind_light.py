7b72f9ce96c9a623429a13e8f555b3e91a430dc5
herwin <h.vanwelbergen@alumnus.utwente.nl>
2020-08-05 22:44:14 +0200

    dcumentation

--- bind_light.py
+++ bind_light.py
@@ -15,6 +15,8 @@ dim_event: integer, optional
     dim_button event (default: 3001)
 undim_event: integer, optional
     undim_button event (default: 2001)
+step_pct: integer, optional
+    dim/undim step size in percent (default:5)
 """
 entity_id = data.get('entity_id')
 event = int(data.get('event'))
@@ -22,6 +24,7 @@ on_events = data.get('on_event',[1002])
 off_events = data.get('off_event',[4002])
 dim_events = data.get('dim_events',[3001, 3002])
 undim_events = data.get('undim_events',[2001, 2002])
+step_pct = data.get('step_pct', 5)
 
 service_data = {'entity_id': entity_id}
 if event in on_events:
@@ -29,8 +32,8 @@ if event in on_events:
 elif event in off_events:
     hass.services.call('light', 'turn_off', service_data, False)
 elif event in dim_events:
-    service_data['brightness_step_pct'] = -5
+    service_data['brightness_step_pct'] = -step_pct
     hass.services.call('light', 'turn_on', service_data, False)    
 elif event in undim_events:
-    service_data['brightness_step_pct'] = 5
+    service_data['brightness_step_pct'] = step_pct
     hass.services.call('light', 'turn_on', service_data, False)
